b727d354b9225aa3d85d3b8bbd0c6a1335af8fc9
vinaykumarpeyala <vinaykumarp216@gmail.com>
2025-02-18 14:54:32 +0530

    commit

---
+++ sample.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    int a = 4;
+    int b = 3;
+    int c;
+    
+    clrscr();
+    c = a + b;
+    printf("Addition of a and b is: %d", c);
+    
+    getch();
+    return 0;
+}
